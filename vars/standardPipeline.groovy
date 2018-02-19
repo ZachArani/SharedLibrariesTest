42d1f9fcaf6d48128979d31f069822a8b50872a1
Zachary Arani <cloud12817@gmail.com>
2018-02-19 12:53:40 -0600

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -12,52 +12,48 @@ def call(body) {
         try {
             stage ('Clone') {
             checkout scm
-        }
-        stage('Build') {
-      parallel {
-        stage('Version') {
-          when {
-            not {
-              environment name: 'createTag', value: ''
             }
-            
-          }
-          steps {
-            echo "${params.createTag}"
-          }
-        }
-        stage('Snapshot') {
-          when {
-            anyOf {
-              environment name: 'buildAsSnapshot', value: 'true'
-              not {
-                environment name: 'head', value: ''
-              }
-              
+            stage('Build') {
+                parallel {
+                    stage('Version') {
+                        when {
+                            not {
+                                environment name: 'createTag', value: ''
+                            } 
+                        }
+                        steps {
+                            echo "${params.createTag}"
+                        }
+                    }
+                    stage('Snapshot') {
+                        when {
+                            anyOf {
+                                environment name: 'buildAsSnapshot', value: 'true'
+                                not {
+                                    environment name: 'head', value: ''
+                                }  
+                            }  
+                        }
+                    steps {
+                        echo "${params.head}"
+                    }
+                }
+                stage('Test') {
+                    when {
+                        allOf {
+                            environment name: 'createTag', value: ''
+                            environment name: 'head', value: ''
+                        }
+                    }
+                    steps {
+                        echo 'Test'
+                    }
+                }
             }
-            
-          }
-          steps {
-            echo "${params.head}"
-          }
         }
-        stage('Test') {
-          when {
-            allOf {
-              environment name: 'createTag', value: ''
-              environment name: 'head', value: ''
-            }
-            
-          }
-          steps {
-            echo 'Test'
-          }
-        }
+    } catch (err) {
+        currentBuild.result = 'FAILED'
+        throw err
       }
-}
-        } catch (err) {
-            currentBuild.result = 'FAILED'
-            throw err
-        }
-    }
+   }
 }
