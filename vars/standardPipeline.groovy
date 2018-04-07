2b0e9b21189404ee021ac98c4c0af2252091167c
Zachary Arani <cloud12817@gmail.com>
2018-04-07 17:16:45 -0500

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -6,54 +6,46 @@ def call(body) {
     body()
 
     node {
-        // Clean workspace before doing anything
-        deleteDir()
-
-        try {
-            stage ('Clone') {
-            checkout scm
-            }
-            stage('Build') {
-                parallel {
-                    stage('Version') {
-                        when {
-                            not {
-                                environment name: 'createTag', value: ''
-                            } 
-                        }
-                        steps {
-                            echo "${params.createTag}"
-                        }
-                    }
-                    stage('Snapshot') {
-                        when {
-                            anyOf {
-                                environment name: 'buildAsSnapshot', value: 'true'
-                                not {
-                                    environment name: 'head', value: ''
-                                }  
-                            }  
-                        }
-                    steps {
-                        echo "${params.head}"
-                    }
+      stages {
+        stage('Build') {
+          parallel {
+            stage('Version') {
+              when {
+                not {
+                  environment name: 'createTag', value: ''
                 }
-                stage('Test') {
-                    when {
-                        allOf {
-                            environment name: 'createTag', value: ''
-                            environment name: 'head', value: ''
-                        }
-                    }
-                    steps {
-                        echo 'Test'
-                    }
-                }
+              }
+              steps {
+                sh 'echo version'
+              }
             }
+            stage('Snapshot') {
+              when {
+                anyOf {
+                  environment name: 'buildAsSnapshot', value: 'true'
+                }
+              }
+              steps {
+                sh 'echo snapshot'
+              }
+            }
+            stage('Test') {
+              when {
+                allOf {
+                  environment name: 'buildAsSnapshot', value: 'false'
+                  environment name: 'createTag', value: ''
+                }
+              }
+              steps {
+                sh 'echo test'
+              }
+            }
+          }
         }
-    } catch (err) {
-        currentBuild.result = 'FAILED'
-        throw err
       }
-   }
+    parameters {
+      string(name: 'createTag', defaultValue: '', description: '')
+      booleanParam(name: 'buildAsSnapshot', defaultValue: false)
+    }
+  }
 }
