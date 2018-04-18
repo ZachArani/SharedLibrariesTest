c90c21e14342558bcf8bcfb6b1f58af7e9e31a14
Zachary Arani <cloud12817@gmail.com>
2018-04-18 11:47:59 -0500

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -10,22 +10,19 @@ def call(body) {
         stage ('Clone') {
             checkout scm
         }
-        stage ('Build') {
-            sh "echo 'building ${buildAsSnapshot} ...'"
-        }
         stage ('Tests') {
-            parallel 'static': {
-                sh "echo 'shell scripts to run static tests...'"
+            parallel 'Version': {
+                sh "echo 'version stuff'"
             },
-            'unit': {
-                sh "echo 'shell scripts to run unit tests...'"
+            'Snapshot': {
+                sh "echo 'snapshot'"
              },
-             'integration': {
-                sh "echo 'shell scripts to run integration tests...'"
+             'Test': {
+                sh "echo 'test'"
               }
            }
-           stage ('Deploy') {
-             sh "echo 'deploying to server ${createTag}...'"
+           stage ('Done') {
+             sh "echo 'We're done I guess'"
            }
         } catch (err) {
             currentBuild.result = 'FAILED'
