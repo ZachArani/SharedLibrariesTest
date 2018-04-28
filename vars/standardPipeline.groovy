44cd521a2628b92548e986462ffc0e0ca69fea59
Zachary Arani <cloud12817@gmail.com>
2018-04-28 15:47:38 -0500

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -10,6 +10,7 @@ def call(body) {
             stage ('Clean') {
                 sh "npx @nti/ci-scripts@micro clean"
             }
+            println params.createTag
             if(!(params.createTag != '') && (params.createTag == '' || "${BRANCH_NAME}" != "master")) {
                 stage ('Prepare') {
                     sh "npx @nti/ci-scripts@micro prepare" 
