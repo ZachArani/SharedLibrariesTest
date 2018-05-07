275756d1e83cf69ff4d212ca577afaf22e5c1892
Zachary Arani <zach.arani@nextthought.com>
2018-05-07 13:35:13 -0500

    removed withCredentials

--- vars/defaultPipeline.groovy
+++ vars/defaultPipeline.groovy
@@ -32,11 +32,6 @@ def call(body) {
                }
            }
             stage("Run") {
-                withCredentials([sshUserPrivateKey(keyFileVariable: 'testFile', credentialsId: '3d8be6d3-d795-4bfc-8962-6a6bd0bbf35d', passphraseVariable: 'GIT_PASS', usernameVariable: 'GIT_USER')]) {
-                    sh "env"
-                    sh "cat testFile"
-                    sh "echo $GIT_USERNAME"
-                }
                 if((params.createTag != null && params.createTag != '') || "${BRANCH_NAME}" == "master") {
                     sh "npx @nti/ci-scripts publish"
                 }
